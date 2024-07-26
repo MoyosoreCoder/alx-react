7fdbe85da1d55b5b129b0fdb0041bc5a5a4296c2
root <root@DESKTOP-F5DEIHC>
2024-07-26 16:55:01 +0100

    new bundle file generated

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,37 +1,18 @@
-// Import jQuery and debounce from Lodash
-import $ from "jquery";
-import debounce from "lodash/debounce";
+const $ = require("jquery");
+const _ = require("lodash");
 
-// Initialize a counter
-let clickCount = 0;
+let counter = 0;
 
-// Function to update the counter and display it
+$("<div id='logo'> </div>").appendTo("body");
+$("<p>Holberton Dashboard</p>").appendTo("body");
+$("<p>Dashboard data for the students</p>").appendTo("body");
+$("<button>Click here to get started</button>").appendTo("body");
+$("<p id='count'></p>").appendTo("body");
+$("<p>Copyright - Holberton School</p>").appendTo("body");
+
 function updateCounter() {
-  clickCount += 1; // Increment the counter
-  $("#count").text(`${clickCount} clicks on the button`); // Update the paragraph content
+  counter++;
+  $("#count").html(`${counter} clicks on the button`);
 }
 
-// Function to add elements to the body
-function addElements() {
-  // Append a paragraph element: "Holberton Dashboard"
-  $("body").append("<p>Holberton Dashboard</p>");
-
-  // Append a paragraph element: "Dashboard data for the students"
-  $("body").append("<p>Dashboard data for the students</p>");
-
-  // Append a button element with the text "Click here to get started"
-  const $button = $("<button>Click here to get started</button>");
-  $("body").append($button);
-
-  // Append a paragraph element with id 'count'
-  $("body").append('<p id="count"></p>');
-
-  // Append another paragraph: "Copyright - Holberton School"
-  $("body").append("<p>Copyright - Holberton School</p>");
-
-  // Bind the debounced updateCounter function to the button click event
-  $button.on("click", debounce(updateCounter, 300));
-}
-
-// Execute the function when the DOM is ready
-$(document).ready(addElements);
+$("button").on("click", _.debounce(updateCounter, 500));
