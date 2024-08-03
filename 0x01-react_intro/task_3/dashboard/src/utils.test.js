ec66a13f23987ece6354d7cfb0346296dcf591e7
Mariam <kelanimaryam1234@gmail.com>
2024-08-03 13:19:53 +0100

    utils.test.js file created

--- 0x01-react_intro/task_3/dashboard/src/utils.test.js
+++ 0x01-react_intro/task_3/dashboard/src/utils.test.js
@@ -1,21 +1,20 @@
 import { getFullYear, getFooterCopy, getLatestNotification } from "./utils";
-describe("utils test", () => {
-  test("getFullYear returns the correct year", () => {
-    const currentYear = new Date().getFullYear();
-    expect(getFullYear()).toBe(currentYear);
+
+describe("Utility function tests", () => {
+  describe("getFullYear", () => {
+    test("returns the current year", () => {
+      const currentYear = new Date().getFullYear();
+      expect(getFullYear()).toBe(currentYear);
+    });
   });
 
-  test("getFooterCopy returns the correct string when the argument is true", () => {
-    expect(getFooterCopy(true)).toBe("Holberton School");
-  });
+  describe("getFooterCopy", () => {
+    test("returns correct string when argument is true", () => {
+      expect(getFooterCopy(true)).toBe("Footer for Index Page");
+    });
 
-  test("getFooterCopy returns the correct string when the argument is false", () => {
-    expect(getFooterCopy(false)).toBe("Holberton School main dashboard");
-  });
-
-  test("getLatestNotification returns the correct string", () => {
-    expect(getLatestNotification()).toBe(
-      "<strong>Urgent requirement</strong> - complete by EOD"
-    );
+    test("returns correct string when argument is false", () => {
+      expect(getFooterCopy(false)).toBe("Footer for Non-Index Page");
+    });
   });
 });
