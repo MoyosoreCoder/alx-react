9651524103eeb1db7debfd27252c4e3762099ef9
Mariam <kelanimaryam1234@gmail.com>
2024-08-09 18:59:35 +0100

    app notification utils folders created

--- 0x01-react_intro/task_5/dashboard/src/app/app.js
+++ 0x01-react_intro/task_5/dashboard/src/app/app.js
@@ -1,56 +1,61 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+<<<<<<< HEAD
+import "./App.css";
+import { getFullYear, getFooterCopy } from "./utils";
+import holberton_logo from "./holberton-logo.jpg";
 
-module.exports = {
-  entry: "./src/index.js", // Entry point for your application
-  output: {
-    filename: "bundle.js", // Output bundle file
-    path: path.resolve(__dirname, "dist"), // Output directory as dist
-    clean: true, // Clean the output directory before emitting
-  },
-  mode: "development", // Set the mode to development
-  module: {
-    rules: [
-      {
-        test: /\.css$/i, // For handling CSS files
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i, // For handling images
-        use: [
-          "file-loader", // Handles image files
-          {
-            loader: "image-webpack-loader", // Optimize images
-            options: {
-              bypassOnDebug: true, // Skip optimization in debug mode
-              disable: true, // Disable optimization
-            },
-          },
-        ],
-      },
-      {
-        test: /\.(js|jsx)$/, // For handling JavaScript and JSX files
-        exclude: /node_modules/,
-        use: ["babel-loader"], // Use Babel loader for transpiling
-      },
-    ],
-  },
-  resolve: {
-    extensions: ["*", ".js", ".jsx"], // Resolve extensions for import statements
-  },
-  devServer: {
-    static: path.resolve(__dirname, "dist"), // Serve content from dist directory
-    compress: true, // Enable gzip compression
-    open: true, // Open the browser after server has started
-    hot: true, // Enable hot module replacement
-    port: 8564, // Use port 8564
-  },
-  devtool: "inline-source-map", // Inline source maps for easier debugging
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: "index.html", // The name of the HTML file to be created
-      template: "./dist/index.html", // Template file for generating the HTML file
-      inject: "body", // Injec the script tag at the end of the body
-    }),
-  ],
-};
+function App() {
+  return (
+    <>
+      <div className="App-header">
+        <img src={holberton_logo} alt="logo" />
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" name="email" />
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" name="password" />
+        <button>OK</button>
+      </div>
+      <div className="App-footer">
+        <p>
+          Copyright {getFullYear()} - {getFooterCopy(true)}
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
+=======
+import "./App.css";
+import { getFullYear, getFooterCopy } from "./utils";
+import holberton_logo from "./holberton-logo.jpg";
+
+function App() {
+  return (
+    <>
+      <div className="App-header">
+        <img src={holberton_logo} alt="logo" />
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" name="email" />
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" name="password" />
+        <button>OK</button>
+      </div>
+      <div className="App-footer">
+        <p>
+          Copyright {getFullYear()} - {getFooterCopy(true)}
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
+>>>>>>> a371e2aa4e6955be86bceb3f09f1702a54731cdc
