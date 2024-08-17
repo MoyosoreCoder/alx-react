ddc9c7131151a169dd7005dddbcc5939ce20fdd0
Mariam <kelanimaryam1234@gmail.com>
2024-08-17 08:35:55 +0100

    1-map.js fixed

--- 0x06-React_Immutable/1-map.js
+++ 0x06-React_Immutable/1-map.js
@@ -1,9 +1,3 @@
-import { Map } from 'immutable';
-
-const getImmutableObject = (object) => Map(object);
-
-export default getImmutableObject;
-
 import { Map } from "immutable";
 
 const getImmutableObject = (object) => Map(object);
